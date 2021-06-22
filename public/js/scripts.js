507d6b0467c70fbc74bebddb1d5844ede399d661
Chinmay Karmokar <chinmaykarmokar@gmail.com>
2021-06-23 01:40:39 +0530

    changes made.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,17 +5,22 @@ let x = 0;
 // Add Items to the list
 
 let insertItems = () => {
+    let username = document.getElementById('items').value;
     newItem = document.getElementById('items').value;
-    itemsToDo.push(`<div>` + newItem + `</div> <button onclick="removeItems(` + x++ + `)">Delete</button>`);
-    document.getElementById('showResults').innerHTML = itemsToDo;
-    console.log(itemsToDo);
+    // itemsToDo.push(`<div>` + newItem + `</div> <button onclick="removeItems(` + x++ + `)">Delete</button>`);
+    // document.getElementById('showResults').innerHTML = itemsToDo;
+    console.log(username + ' has inserted ');
 }
 
 // Remove Items from the list
 
-let removeItems = (id) => {
-   let arrayIndex = id;
-   let itemToBeRemoved = itemsToDo.splice(arrayIndex,1);
-   console.log(itemToBeRemoved);
-   console.log(itemsToDo);
+let removetodoItem = (id) => {
+    alert(document.getElementById(id).innerHTML);
 }
+
+// let removeItems = (id) => {
+//    let arrayIndex = id;
+//    let itemToBeRemoved = itemsToDo.splice(arrayIndex,1);
+//    console.log(itemToBeRemoved);
+//    console.log(itemsToDo);
+// }
