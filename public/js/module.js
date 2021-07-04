2e21a5aab6d1184c554efafe2993bf7d8a428346
Chinmay Karmokar <chinmaykarmokar@gmail.com>
2021-07-04 18:18:16 +0530

    resolved few bugs.

---
+++ public/js/module.js
@@ -0,0 +1,9 @@
+module.exports = {
+    host: "sql6.freemysqlhosting.net",
+    user: "sql6406059",
+    password: "xS6yR7ljMf",
+    database: "sql6406059",
+    email: "todobychinmay@gmail.com",
+    pass: "!champion",
+    name: "Chinmay Karmokar"
+}
